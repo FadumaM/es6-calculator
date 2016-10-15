f5af8f62636eca1008a7492b774abc0b2578ed5d
FadumaM <faduma.mohammed@gmail.com>
2016-10-16 00:35:29 +0100

    files setup done

---
+++ dist/js/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// class User {
+//   constructor(name, isAdmin) {
+//     this.name = name;
+//     this.isAdmin = isAdmin;
+//   }
+// }
+//
+// let users = [
+//   new User("Faduma", true),
+//   new User("Aisha", false),
+//   new User("Asmaa", false)
+// ];
+//
+// console.log(users.find(user => user.isAdmin));
+
+var a = 5;
+var b = 27;
+
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+// class User {
+//   constructor(name, isAdmin) {
+//     this.name = name;
+//     this.isAdmin = isAdmin;
+//   }
+// }
+//
+// let users = [
+//   new User("Faduma", true),
+//   new User("Aisha", false),
+//   new User("Asmaa", false)
+// ];
+//
+// console.log(users.find(user => user.isAdmin));
+
+let a = 5;
+let b = 27;
+
+console.log(`Fifteen is ${a + b} and\nnot ${2 * a + b}.`);
